42e4d08a009ea4e10e957194bc17ac1b0bbf728d
gabrielqt <gabriel.traiano4@gmail.com>
2026-03-15 14:35:57 -0300

    ajusted JPAUtil, created threadlocal

--- src/main/java/com/guildaSys/config/JPAUtil.java
+++ src/main/java/com/guildaSys/config/JPAUtil.java
@@ -1,4 +1,4 @@
-package com.bank.config;
+package com.guildaSys.config;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
@@ -9,7 +9,23 @@ public class JPAUtil {
     private static final EntityManagerFactory emf =
             Persistence.createEntityManagerFactory("bankPU");
 
+    private static final ThreadLocal<EntityManager> threadLocal = new ThreadLocal<>();
+
     public static EntityManager getEntityManager() {
-        return emf.createEntityManager();
+        EntityManager em = threadLocal.get();
+        if (em == null || em.isOpen()) {
+            em = emf.createEntityManager();
+            threadLocal.set(em);
+        }
+        return em;
     }
+
+    public static void closeEntityManager() {
+        EntityManager em = threadLocal.get();
+        if (em != null ||  em.isOpen()) {
+            em.close();
+        }
+        threadLocal.remove();
+    }
+
 }
